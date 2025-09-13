cbf5a28b9283349cff206ef2fc7a23f7e0a79b6f
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2025-09-12 17:42:06 -0700

    Added commented old code and deleted unused function

--- src/main/java/frc/robot/commands/CoralIntakeCommand.java
+++ src/main/java/frc/robot/commands/CoralIntakeCommand.java
@@ -8,6 +8,66 @@ import frc.robot.subsystems.intake.CoralIntake;
 import frc.robot.subsystems.intake.IntakeConstants;
 
 public class CoralIntakeCommand extends Command {
+  // private Boolean eject;
+
+  // public Boolean getEject() {
+  //   return eject;
+  // }
+
+  // public void setEject(Boolean eject) {
+  //   this.eject = eject;
+  // }
+
+  // public CoralIntakeCommand() {
+  //   addRequirements(CoralIntake.getInstance());
+  //   this.eject = false;
+  // }
+
+  // @Override
+  // public void initialize() {
+  //   CoralIntake.getInstance().setVelocity(LinearVelocity.ofBaseUnits(4, MetersPerSecond));
+  // }
+
+  // @Override
+  // public void execute() {
+  //   if (eject) {
+  //     CoralIntake.getInstance()
+  //         .setVelocity(
+  //             LinearVelocity.ofBaseUnits(
+  //                 IntakeConstants.CoralIntake.outtakeVelocity, MetersPerSecond));
+  //   } else {
+  //     // If Coral is in the "Jammed" position- covering all three top sensors
+  //     if (CoralIntake.getInstance().getSensor1()
+  //         && CoralIntake.getInstance().getSensor2()
+  //         && CoralIntake.getInstance().getSensor3()) {
+  //       CoralIntake.getInstance().setVelocityShift(LinearVelocity.ofBaseUnits(6, MetersPerSecond));
+  //       // If Coral is in intake-ready position
+  //     } else if ((CoralIntake.getInstance().getSensor1() || CoralIntake.getInstance().getSensor3())
+  //         && CoralIntake.getInstance().getSensor2()) {
+  //       CoralIntake.getInstance().setVelocity(LinearVelocity.ofBaseUnits(8, MetersPerSecond));
+  //       // Deafult intake velocity
+  //     } else {
+  //       CoralIntake.getInstance().setVelocity(LinearVelocity.ofBaseUnits(4, MetersPerSecond));
+  //     }
+  //   }
+  // }
+
+  // @Override
+  // public void end(boolean interrupted) {
+  //   CoralIntake.getInstance().setVelocity(LinearVelocity.ofBaseUnits(0, MetersPerSecond));
+  // }
+
+  // @Override
+  // public boolean isFinished() {
+  //   if (eject) {
+  //     // returns when the Coral has been scored
+  //     return !CoralIntake.getInstance().getSensor4();
+  //   } else {
+  //     // returns when the Coral is in position
+  //     return CoralIntake.getInstance().hasCoral();
+  //   }
+  // }
+
   public enum IntakeMode {
     OUTTAKE,
     STOP,
@@ -55,9 +115,5 @@ public class CoralIntakeCommand extends Command {
   @Override
   public void end(boolean interrupted) {
     CoralIntake.getInstance().setVelocity(LinearVelocity.ofBaseUnits(0, MetersPerSecond));
-  }
-
-  public void setVelocity() {
-    
   }
 }
